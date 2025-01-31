0158bbb3f042792198895ae3e034956a8526ed8d
Bhuneshvar <imankitkalirawana@gmail.com>
2025-01-31 20:07:59 +0530

    Time: 2 ms (35.78%) | Memory: 52.3 MB (68.53%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,11 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === nums[i + 1]) {
-            nums.splice(i, 1)
-            i--
+    let w = 1;
+
+    for (let r = 1; r < nums.length; r++) {
+        if (nums[r] !== nums[r - 1]) {
+            nums[w] = nums[r];
+            w++;
         }
     }
-    return nums.length
+
+    return w;
 };
