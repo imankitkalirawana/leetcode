0912158e9509305ef054ee9c637d8debf1c8fcb5
Bhuneshvar <imankitkalirawana@gmail.com>
2025-02-09 21:41:35 +0530

    Time: 15 ms (92.40%) | Memory: 55.7 MB (90.50%) - LeetSync

---
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var partitionString = function (s) {
+    let result = 1;
+    let window = s[0];
+
+    for (let i = 1; i < s.length; i++) {
+        if (window.includes(s[i])) {
+            result++;
+            window = s[i];
+        } else {
+            window += s[i];
+        }
+    }
+    return result
+};
