c487e2916608482eafdec08051a7a6801cefd2aa
Bhuneshvar <imankitkalirawana@gmail.com>
2025-02-22 13:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (8.62%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+    if (str1 + str2 !== str2 + str1) return '';
+
+    let a = str1.length
+    let b = str2.length
+
+    while (b) {
+        let temp = b; // 4
+        b = a % b; // 2
+        a = temp; // 4 
+    }
+
+    return str1.substring(0, a)
+
+};
