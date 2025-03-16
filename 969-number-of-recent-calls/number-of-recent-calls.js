dc29b2fd5dce000bfd248e72961f7cd7ea41c7e0
Bhuneshvar <imankitkalirawana@gmail.com>
2025-03-16 08:09:21 +0530

    Time: 38 ms (50.42%) | Memory: 69.8 MB (14.93%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.js
+++ 969-number-of-recent-calls/number-of-recent-calls.js
@@ -14,9 +14,3 @@ RecentCounter.prototype.ping = function(t) {
     this.queue.push(t);
     return this.queue.length;    
 };
-
-/** 
- * Your RecentCounter object will be instantiated and called as such:
- * var obj = new RecentCounter()
- * var param_1 = obj.ping(t)
- */
